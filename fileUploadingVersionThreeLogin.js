a54a88dcdce788307e1045f41c8e225f7ecbb093
oleksandr.ivashchenko <oleksandr.ivashchenko@bigcommerce.com>
2020-10-22 10:23:39 +0300

    learning(node js work with excel + authentication) I have added authentication from my node js passport login project

--- fileUploadingVersionThreeLogin.js
+++ fileUploadingVersionThreeLogin.js
@@ -1,3 +1,10 @@
+if (process.env.NODE_ENV !== 'production') {
+    /**
+     * Here we add our .env file
+     */
+    require('dotenv').config()
+}
+
 const express = require('express');
 const upload = require('express-fileupload');
 const fs = require("fs");
@@ -5,13 +12,46 @@ var url = require('url');
 const path = require('path');
 const download = require("download");
 var ROOT = __dirname + "/downloads";
-
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 
 /**
  * After start the server - you can download the file with using this url:
  * http://localhost:3000/newDataFile.xlsx?secret=o_O
  */
 
+/**
+ * Every of these express libraries should be installed. I mean "npm I express -D; npm I express-fileupload -D"
+ * @type {*|Express}
+ */
+const app = express();
+
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 /**
  * Функция Next - если она есть в запросе - то она вызывает именно следующую middleware функцию. Она не вызывает обычную функцию.
  * Только middleware(у которой есть запрос и ответ). Можно называть эту функцию любым именем, но чтобы не путаться -
@@ -33,11 +73,7 @@ var ROOT = __dirname + "/downloads";
 
 
 
-/**
- * Every of these express libraries should be installed. I mean "npm I express -D; npm I express-fileupload -D"
- * @type {*|Express}
- */
-const app = express();
+
 /**
  * app.use - функция которая будет использовать определенную другую функцию при каждом запросе(Но это не точно - возможно только для того
  * запроса перед которым app.use стоит или после запроса если app.use стоит после этого запроса). Например app.use upload -
@@ -60,15 +96,62 @@ const app = express();
 
 app.use(upload());
 
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
 
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+    console.log(users);/*I have placed it there to just have fun seeing in realtime
+   what have come to the array*/
+})
+
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
 
 /**
  * There we will connect this js file with the html file. SO when browser opens a page(makes a get request) - there will be returned our html file./
  * That how we works with js and html files.
  * The middleware is anything that contains request and responce
  */
-app.get('/', (req, res) => {
+app.get('/', checkAuthenticated, (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
---
+++ passport-config.js
@@ -0,0 +1,97 @@
+/**
+ * This file imported from my project node-js-passport-login.
+ * It will be used here with fileUploadingVersionThreeLogin.js
+ */
+
+
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
+    const user = getUserByEmail(email)
+    /**
+     * if user hasn't been found in the users array(array imitate DataBase) by his email - this user doesn't exist in the "DataBase".
+     * Done - is a function that we call when we will authenticate user (successfully for user or not. In this
+     * function done will return or user email or done with error message
+
+     * The first argument for done function is error which we can recieve there.
+     * As far as now we don't have an error - we will
+     * pass "null" there. The second parameter - is in our case "user we found there (taken from users massive
+     * which was given to this method in arguments(method has been called from passport-config)", third parameter
+     * is message which will be exposed on the page if we didn't find that user(message with which this function
+     * will be ended and message which will be returned.).
+     */
+    if (user == null) {
+      return done(null, false, { message: 'No user with that email' })
+    }
+
+    try {
+      /**
+       * If hashed password on the array and password from request matched -
+       * there will be successfull function "done". To this function we will return variable user which contains
+       * user email which we want to authenticate.
+       * http://www.passportjs.org/docs/configure/ - there is a really good, claryfying example here.
+       * Also done is asynchronous function, when "return" is also asynchronous - or not? Then it would be a really good difference
+       * between done and "return" keyword.
+
+       * If password's didn't match -
+       * will be used block "else" where no user will be returned. Instead will be
+       * returned message 'Password incorrect' which user will see in the browser on "/login" link.
+
+       * bcrypt.compare can compate even simple not hashed data with hashed data.
+       */
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user)
+      } else {
+        return done(null, false, { message: 'Password incorrect' })
+      }
+    } catch (e) {
+      return done(e)
+      /**
+       * Функция вернет done который будет содержать имя юзера. Это имя будет передано
+       * из этой callback функции authenticateUser в функцию passport.use ниже
+       * passport.use сравнивает email который прилетел из запроса и email который вернется authenticateUser
+       * функция. AuthenticateUser функция возвращает email ТОЛЬКО в случае если пароль из хэша(в бд) и пароль
+       * присланный в запросе сопадают. Если она не вернет user.email - тогда функция passport.use будет провалена.
+       */
+    }
+  }
+
+  /**
+   * Вторым аргументом передана функция аутентификации. Это функция passport.local strategy.
+   * Первым параметром берет usernameField(кстати вторым значением в json объекте можно было-бы
+   * передать и пароль - но он уже передан в другой функции) и с usernameField вторым параметром
+   * вызывает callback authenticateUser - функцию(через callback).
+   *  authenticateUser после прохождения в ней аутентификация в случае успеха возвращает user email и passport.use
+   *  сравнивает его через local strategy с user email из бд и соответственно делает
+   *  return done (который "true") или - если в функции authenticateUser аутентификация не была успешной -
+   *  она вернет сообщение об ошибки - тогда после сравнения сообщения с user email из бд -
+   *  здесь аутентификация не пройдет и таким образом юзер НЕ БУДЕТ
+   *  аутентифицирован.
+
+   *  В passport.use localstrategy можно так же передавать и логин и пароль в JSON
+   *  формате и они сравниваются с логином и паролем из бд(но так как у нас как бэ сравнивание хэшированных
+   *  данных - мы его проводим в другой функции и реализуем так как я описал).
+   */
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
+
+  /**
+   * This function makes serializing - which means "to store in one(during one) session"
+   * For make this whole system work - we need to open this session and to call the done function
+   * with arguments of user.id which use login method now.
+   */
+  passport.serializeUser((user, done) => done(null, user.id))
+
+  /**
+   * This function makes deserializing. Serializing means "to store in one(during one) session", so this will
+   * close our session.
+   * For make this whole system STOP work - we need to CLOSE this session and to call the done function
+   * with arguments of user.id which is now using this system.
+   */
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+  })
+}
+
+module.exports = initialize
